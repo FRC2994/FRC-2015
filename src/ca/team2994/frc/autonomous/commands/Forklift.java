3724fdfa55ed939001b7f99c5fdce5d6fc461190
eandr127 <rebl10@hotmail.com>
2015-02-16 14:43:55 -0500

    Added forklift command

---
+++ src/ca/team2994/frc/autonomous/commands/Forklift.java
@@ -0,0 +1,34 @@
+package ca.team2994.frc.autonomous.commands;
+
+import ca.team2994.frc.autonomous.AutoCommand;
+import ca.team2994.frc.robot.Subsystems;
+
+public class Forklift implements AutoCommand {
+
+	private final int level;
+	
+	public Forklift(int level) {
+		this.level = level;
+	}
+	
+	@Override
+	public void initialize() {
+		Subsystems.forklift.setLevel(level);
+		
+	}
+
+	@Override
+	public boolean tick() {
+		if(!Subsystems.forklift.isLevelReached()) {
+			Subsystems.forklift.update();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void cleanup() {
+		Subsystems.forklift.stop();
+	}
+
+}
