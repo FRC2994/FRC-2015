38511bfc08282d54bb28fd2c29691fbed8c0cb71
eandr127 <rebl10@hotmail.com>
2015-02-28 14:26:35 -0500

    Do some autonomous reformatting, add new auto mode

--- src/ca/team2994/frc/autonomous/modes/DriveToAutoZone.java
+++ src/ca/team2994/frc/autonomous/modes/DriveToAutoZone.java
@@ -4,6 +4,7 @@ import ca.team2994.frc.autonomous.AutoBuilder;
 import ca.team2994.frc.autonomous.AutoCommand;
 import ca.team2994.frc.autonomous.AutoMode;
 import ca.team2994.frc.autonomous.commands.DriveStraight;
+import ca.team2994.frc.autonomous.commands.GearShift;
 
 public class DriveToAutoZone extends AutoMode {
 	
@@ -15,6 +16,9 @@ public class DriveToAutoZone extends AutoMode {
 		// This assumes that we start in the staging zone parallel to the closest scoring platform
 		AutoBuilder builder = new AutoBuilder();
 
+		// Shift to low gear
+		builder.add(new GearShift(false));
+		
 		// Drive forward into the zone.
 		builder.add(new DriveStraight(STRAIGHT_DISTANCE));
 		
